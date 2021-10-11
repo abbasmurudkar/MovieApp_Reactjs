26ce87afd0b21b1c63937102006de05fdd5f44a6
abbasmurudkar <abbasmurudkar18@icloud.com>
2021-10-11 13:30:18 +0530

    Reactjs

--- src/misc/Api.js
+++ src/misc/Api.js
@@ -1,6 +1,6 @@
-const ApiBase_Url = "https://api.tvmaze.com";
+const ApiBase_Url = 'https://api.tvmaze.com';
 
 export async function getapi(querystring){
-    const api = await fetch(`${ApiBase_Url}${querystring}`).then(r=>r.json())
-    return api
+    const Api =await fetch(`${ApiBase_Url}${querystring}`).then((r)=>r.json())
+    return Api
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,64 +3,71 @@ import Mainpage from '../components/Mainpage'
 import { useState } from 'react'
 import {getapi} from '../misc/Api'
 const Home = () => {
-    const [text, settext] = useState('')
-    const [result, setresult] = useState(null)
-    const [actors, setactors] = useState('shows')
-    const ischecked = actors === "shows";
-    const Inputtext = (e) => {
-        settext(e.target.value)
+const [text, settext] = useState('')
+const [results, setresult] = useState(null)
+const [searchoptions, setsearchoptions] = useState('shows')
+const ischecked = searchoptions === "shows";
+const Inputtext = (ev)=>{
+    settext(ev.target.value)
+}
+const OnSearch = ()=>{
+    getapi(`/search/${searchoptions}?q=${text}`).then(result=>{
+        setresult(result)
+        console.log(result)
+    })
+    // fetch(`https://api.tvmaze.com/search/shows?q=${text}`).then((r)=>r.json()).then((result)=>{
+        // console.log(result)
+}
+const onkeydown = (ev) =>{
+    if(ev.keyCode === 13)
+    {
+        OnSearch()
     }
-    const Onsearch = () => {
-        getapi(`/search/${actors}?q=${text}`).then((result)=>{
-        // fetch(`https://api.tvmaze.com/search/shows?q=${text}`).then(r => r.json()).then(result => {
-        //     console.log(result)
-            setresult(result)
-        })
-    }
-    const onKeyDown = ev => {
-        console.log(ev.KeyCode)
-    };
-    const DisplayResult = () => {
-        if (result && result.length === 0) {
-            return(
-            <div>No Matches Found</div>
-            )
-        }
-        if (result && result.length > 0) {
-           return result[0].show ? result.map((items)=>{
-            return(
-                <div key={items.show.id}>
-                    {items.show.name}
-                </div>
-            )
-        }):
-        result.map((items)=>{
-            return(
-                <div key={items.person.id}>
-                    {items.person.name}
-                </div>
-            )
-        })
-        }
-    }
-    const onRadiochange = (ev) =>{
-setactors(ev.target.value)
-    }
-    console.log(actors)
+}
+const DisplayResult = ()=>{
+if(results && results.length === 0)
+{
+    return(
+        <div>No mathces found</div>
+    )
+}
+if(results && results.length > 0)
+{
+    return results[0].show ? results.map((items)=>{
+        return(
+            <div key={items.show.id}>
+                {items.show.name}
+            </div>
+        )
+       }):
+       results.map((items)=>{
+        return(
+            <div key={items.person.id}>
+                {items.person.name}
+            </div>
+        )
+       })
+}
+return null
+}
+const radiochange = (ev) =>{
+    setsearchoptions(ev.target.value)
+}
+console.log(searchoptions)
     return (
         <Mainpage>
-            <input type="text" placeholder="Search here" onChange={Inputtext} onKeyDown={onKeyDown} text={text}></input>
-            <button type="button" onClick={Onsearch}>Search</button>
+            <input type="text" onChange={Inputtext} text={text} onKeyDown={onkeydown} ></input>
+            <button type="button" onClick={OnSearch}>Search</button>
             {DisplayResult()}
             <div>
-                <label htmlFor="show-movies">
-                    Movie
-                    <input type="radio" id="show-movies"checked={ischecked} onChange={onRadiochange} value="shows"/>
-                </label>
-                <label htmlFor="show-actors">
-                    Actors
-                    <input type="radio" id="show-actors" checked={!ischecked} onChange={onRadiochange} value="people"/>
-                </label>
+            <label htmlFor="search-movies">
+                Movies
+                <input id="search-movies" checked={ischecked} type="radio" value="shows" onChange={radiochange} />
+            </label>
+            <label htmlFor="search-actors">
+                Actors
+                <input id="search-actors" checked={!ischecked}type="radio" value="people" onChange={radiochange}/>
+            </label>
             </div>
         </Mainpage>
     );
