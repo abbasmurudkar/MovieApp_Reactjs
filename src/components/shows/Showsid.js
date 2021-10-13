45a767868ac94ed7e11ff5e734f535f48863a670
abbasmurudkar <abbasmurudkar18@icloud.com>
2021-10-13 19:32:37 +0530

    Reactjs

--- src/components/shows/Showsid.js
+++ src/components/shows/Showsid.js
@@ -4,13 +4,37 @@ import { getapi } from '../../misc/Api';
 const Showsid = () => {
     const {id} = useParams();
     const [show, setshow] = useState(null)
+    const [isloading, setisloading] = useState(true)
+    const [error, seterror] = useState(null)
     useEffect(()=>{
+
+        let ismount = true;
 getapi(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result=>{
-    console.log(result)
-    setshow(result)
+        if(ismount){
+            console.log(result)
+            setshow(result)
+            setisloading(false)
+        }
 })
+.catch(err=>{
+    if(ismount){
+        seterror(err.message)
+        setisloading(false)
+    }
+})
+return () =>{
+    ismount=false
+}
     },[id])
 
+if(isloading)
+{
+    return <div>Data is being Loaded</div>
+}
+if(error)
+{
+    return <div>Error Message:{error}</div>
+}
     return (
         <div>
             this is show page
